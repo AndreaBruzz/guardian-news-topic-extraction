33927e2ed423d8603a0bb6f9e895fa2ac379b19d
AndreaBruzz <andrea.bruttomesso.1@studenti.unipd.it>
2024-05-23 20:30:34 +0200

    spring initializr

---
+++ monitoring-service/src/main/java/dei/unipd/it/demo/ArticlesMonitoringApplication.java
@@ -0,0 +1,13 @@
+package dei.unipd.it.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesMonitoringApplication.class, args);
+	}
+
+}
---
+++ monitoring-service/src/test/java/dei/unipd/it/demo/ArticlesMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package dei.unipd.it.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
