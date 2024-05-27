2ca3bb95709aeca29da25e37b89e7dc486cfe36d
AndreaBruzz <andrea.bruttomesso.1@studenti.unipd.it>
2024-05-27 21:27:48 +0200

    test: app

---
+++ articlesmonitor/src/test/java/it/dei/unipd/AppTest.java
@@ -0,0 +1,12 @@
+package it.dei.unipd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
