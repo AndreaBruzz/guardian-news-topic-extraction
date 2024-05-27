3e472ec243819aa7f2dd2185d7ea78b649987145
AndreaBruzz <andrea.bruttomesso.1@studenti.unipd.it>
2024-05-27 21:28:58 +0200

    articles monitor

---
+++ articlesmonitor/src/main/java/it/dei/unipd/articlesmonitor/ArticlesMonitorApplication.java
@@ -0,0 +1,11 @@
+package it.dei.unipd.articlesmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesMonitorApplication.class, args);
+    }
+}
