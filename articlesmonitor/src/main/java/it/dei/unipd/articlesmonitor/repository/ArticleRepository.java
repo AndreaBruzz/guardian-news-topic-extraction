e21c5b51f99d12d6804d5697891afbfc2c8e716d
AndreaBruzz <andrea.bruttomesso.1@studenti.unipd.it>
2024-07-20 15:16:44 +0200

    empty repo

--- articlesmonitor/src/main/java/it/dei/unipd/articlesmonitor/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package it.dei.unipd.articlesmonitor.repository;
-
-import it.dei.unipd.articlesmonitor.model.Article;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends MongoRepository<Article, String> {
-}
