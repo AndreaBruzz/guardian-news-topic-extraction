8839a70300d33ae46a74ea439409333e3d82b65e
AndreaBruzz <andrea.bruttomesso.1@studenti.unipd.it>
2024-08-11 18:03:53 +0200

    add topic modeling to topics controller

--- monitor-service/src/main/java/com/unipd/monitor/controllers/TopicsController.java
+++ monitor-service/src/main/java/com/unipd/monitor/controllers/TopicsController.java
@@ -1,13 +1,18 @@
 package com.unipd.monitor.controllers;
 
 import com.unipd.monitor.models.TopicsRequestDTO;
+import com.unipd.monitor.models.TopicDTO;
+import com.unipd.monitor.models.TopicResponseDTO;
 import com.unipd.monitor.services.ElasticsearchService;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
@@ -15,23 +20,38 @@ import org.springframework.web.bind.annotation.*;
 public class TopicsController {
 
     private final ElasticsearchService elasticsearchService;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public TopicsController(ElasticsearchService elasticsearchService) {
+    public TopicsController(ElasticsearchService elasticsearchService, RestTemplate restTemplate) {
         this.elasticsearchService = elasticsearchService;
+        this.restTemplate = restTemplate;
     }
 
     @PostMapping("/topics")
-    public ResponseEntity<?> getTopics(@RequestBody TopicsRequestDTO topicsRequest) {
+    public ResponseEntity<TopicResponseDTO> getTopics(@RequestBody TopicsRequestDTO topicsRequest) {
         String collectionId = topicsRequest.getCollectionId();
         String query = topicsRequest.getQuery();
+        int numTopics = topicsRequest.getNumOfTopics();
+        int numWords = topicsRequest.getNumOfTopWords();
 
         ArrayList<String> articles = elasticsearchService.searchArticles(collectionId, query);
-        
-        for (String article : articles) {
-            System.out.println(article);
+
+        String malletServiceUrl = "http://mallet-service:8080/mallet/topics?numTopics=" + numTopics + "&numWords=" + numWords;
+        List<Map<String, Object>> rawTopics = restTemplate.postForObject(malletServiceUrl, articles, List.class);
+
+        List<TopicDTO> topics = new ArrayList<>();
+        for (int i = 0; i < rawTopics.size(); i++) {
+            Map<String, Object> rawTopic = rawTopics.get(i);
+            List<String> topWords = (List<String>) rawTopic.get("topic");
+            topics.add(new TopicDTO(i, topWords));
         }
 
-        return ResponseEntity.ok("OK");
+        TopicResponseDTO response = new TopicResponseDTO();
+        response.setQuery(query);
+        response.setTopics(topics);
+
+        System.out.println(response);
+        return ResponseEntity.ok(response);
     }
 }
