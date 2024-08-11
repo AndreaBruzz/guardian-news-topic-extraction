a82b4333c112a39b2fc949fbf2f48982f8a0ba6d
AndreaBruzz <andrea.bruttomesso.1@studenti.unipd.it>
2024-08-11 18:01:20 +0200

    mallet service init

---
+++ mallet-service/src/main/java/com/unipd/mallet/MalletApplication.java
@@ -0,0 +1,13 @@
+package com.unipd.mallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalletApplication.class, args);
+	}
+
+}
---
+++ mallet-service/src/test/java/com/unipd/mallet/MalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipd.mallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
